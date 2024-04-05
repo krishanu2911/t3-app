32ef3732b0806528cbc4106ebca9eb05e18fd9c8
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2024-04-05 17:48:57 +0530

    feat: signup api

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,10 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { generateOtp } from "@/utils/utilFunc";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { TRPCError } from "@trpc/server";
+
 
 export const userRouter = createTRPCRouter({
   signUp: publicProcedure.input(
@@ -11,7 +15,37 @@ export const userRouter = createTRPCRouter({
     }),
   ).mutation(async ({ctx , input}) => {
     const newOtp = generateOtp();
+    const hashedPassword = await bcrypt.hash(input.password, 10);
+    // console.log("")
 
+    try {
+     const newUser = await ctx.db.user.create({
+      data: {
+        name: input.name,
+        email: input.email,
+        password: hashedPassword,
+        otp: newOtp,
+      }
+    })
+    
+    return {
+      message: "send otp for verification",
+      user: newUser,
+      otp: newUser.otp
+    } 
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') {
+        throw new TRPCError({
+          code:"CONFLICT",
+          message:"An account with this email already exists."
+        })
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "error durring signup."
+        })
+      }
+    }
     
     
 
