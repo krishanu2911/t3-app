9f2becf95c7538287d36fd783c77f6465540abde
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2024-04-06 00:04:47 +0530

    feat: verify otp and login uer api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { api } from "@/utils/api";
 
 export default function Home() {
   const signUpUserMutation = api.user.signUp.useMutation();
+  const loginMutation = api.user.login.useMutation();
 
   const signInUser = async (name: string , email: string, password: string) => {
     try {
@@ -17,13 +18,23 @@ export default function Home() {
     }
   }
 
+  const loginInUser = async () => {
+    try {
+      const data = await loginMutation.mutateAsync({email: "test1@gmail.com" , password: "test1@1234"});
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <Head>
         <title>Create T3 App</title>
       </Head>
       <main className=" w-screen border-2 border-red-500 min-h-screen">
-        
+        <button onClick={() => signInUser("test1","test1@gmail.com", "test1@1234")}>SignUp</button>
+        <button onClick={loginInUser}>Login</button>
       </main>
     </>
   );
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,7 +2,6 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { generateOtp } from "@/utils/utilFunc";
 import { z } from "zod";
 import bcrypt from "bcrypt";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { TRPCError } from "@trpc/server";
 
 
@@ -14,40 +13,131 @@ export const userRouter = createTRPCRouter({
       password: z.string().min(6),
     }),
   ).mutation(async ({ctx , input}) => {
+    const existedUser = await ctx.db.user.findUnique({
+      where: {email: input.email}
+    })
+
+    if(existedUser && existedUser.isVerified) {
+
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Already created account with this email."
+        })
+    }
+
     const newOtp = generateOtp();
     const hashedPassword = await bcrypt.hash(input.password, 10);
-    // console.log("")
 
     try {
-     const newUser = await ctx.db.user.create({
-      data: {
-        name: input.name,
-        email: input.email,
-        password: hashedPassword,
-        otp: newOtp,
+      if (existedUser && !existedUser.isVerified) {
+        const updateUser = await ctx.db.user.update({
+          where: {email: input.email},
+          data: {
+            name: input.name,
+            otp: newOtp,
+            password: hashedPassword
+          }
+        });
+
+        return {
+          message: "Resent otp.",
+          otp: updateUser.otp
+        }
       }
-    })
-    
-    return {
-      message: "send otp for verification",
-      user: newUser,
-      otp: newUser.otp
-    } 
+
+      const newUser = await ctx.db.user.create({
+        data: {
+          name: input.name,
+          email: input.email,
+          password: hashedPassword,
+          otp: newOtp,
+        },
+      });
+  
+      return {
+        message: "OTP sent for verification.",
+        otp: newUser.otp,
+      };
+
     } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') {
-        throw new TRPCError({
-          code:"CONFLICT",
-          message:"An account with this email already exists."
-        })
-      } else {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "error durring signup."
-        })
-      }
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "An error occurred during signup.",
+      });
     }
-    
-    
 
   }),
+  verifyOtp: publicProcedure
+  .input(z.object({email: z.string().email(), clientOtp: z.string()}))
+  .mutation(async ({ctx , input}) => {
+    try {
+
+      const user = await ctx.db.user.findUnique({
+        where: {email: input.email}
+      });
+
+      if(!user || user.otp !== input.clientOtp) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Invalid email or otp"
+        })
+      }
+
+      await ctx.db.user.update({
+        where: {
+          email: input.email
+        },
+        data: {
+          otp: null,
+          isVerified: true
+        }
+      })
+
+      return {
+        message: "otp verified!!!"
+      }
+    } catch (error) {
+      throw error
+    }
+  }),
+  login: publicProcedure.input(
+    z.object({email: z.string().email(), password: z.string()})
+  )
+  .mutation(async ({ctx , input}) => {
+
+    try {
+     const userDetails = await ctx.db.user.findUnique({
+      where: {
+        email: input.email
+      }
+    })
+
+    if(!userDetails) {
+
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Invalid email or passwor.d"
+      })
+    }
+
+    const passwordCorrectFlag = await bcrypt.compare(input.password , userDetails.password);
+
+    if (passwordCorrectFlag) {
+      return {
+        message: "login successfull!!!",
+        success: true
+      }
+    } else {
+      return {
+        message: "incorrect password",
+        success: false
+      }
+      
+    } 
+    } catch (error) {
+
+      throw error
+    }
+
+  })
 });
